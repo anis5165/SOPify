0a4b8f9a1a8b3429e82c6041c13abf8f06b3ffa2
anis5165 <anishkumargvm02@gmail.com>
2025-06-02 11:27:03 +0530

    fix: update CORS configuration to allow requests from any origin

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 // Set payload size limits - increase these values for handling large images
 
 // middleware
-app.use(cors({origin: "http://localhost:3000"}));
+app.use(cors({origin: "*"}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/user', UserRouter);
