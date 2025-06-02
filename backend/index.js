23710dd0d998b1f3f15cae258ca2c47e6c16a59f
anis5165 <anishkumargvm02@gmail.com>
2025-06-02 11:28:52 +0530

    fix: update CORS middleware to allow requests from any origin and specify allowed methods

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,11 @@ const port = process.env.PORT || 5000;
 // Set payload size limits - increase these values for handling large images
 
 // middleware
-app.use(cors({origin: "*"}));
+app.use(cors({
+    origin: "*" || "https://sopify-five.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use('/user', UserRouter);
